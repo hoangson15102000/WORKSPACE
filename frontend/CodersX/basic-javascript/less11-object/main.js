498dc1ef027edc81e96c8e348861ad18bac18d48
hoangson15102000 <nguyenhoangsonasus@gmail.com>
2022-07-17 10:08:54 +0700

    2

---
+++ frontend/CodersX/basic-javascript/less11-object/main.js
@@ -0,0 +1,10 @@
+// object methods các phương thức object
+var myDog = {
+    weight: 5,
+    name: 'đần',
+    age: 1,
+    bark: function () {// anonymous functions
+        console.log(' mèo mèo')
+    }
+}
+console.log(myDog)
